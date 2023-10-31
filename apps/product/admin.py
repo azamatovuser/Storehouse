8a9c915c4ecd27fe05152d87b390f15ac9ee426c
Azamat <azamatovuser@gmail.com>
2023-10-31 08:00:29 +0300

    patch 0.0.1

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Place, Category, Product, Storehouse
 
-# Register your models here.
+
+admin.site.register(Place)
+admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'is_active')
+    readonly_fields = ('created_date',)
+    list_filter = ('category', 'is_active')
+
+
+@admin.register(Storehouse)
+class StorehouseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'account', 'product', 'place')
+    readonly_fields = ('created_date',)
+    list_filter = ('account', 'place')
--- apps/product/models.py
+++ apps/product/models.py
@@ -17,13 +17,22 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
     title = models.CharField(max_length=221)
     description = models.TextField()
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Storehouse(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.account}'s product - {self.product}"
